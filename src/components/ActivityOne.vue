e383efc923ac2c9d99eff1bd0b0b3a21ca83142d
oijiemae <oijiemae228@gmail.com>
2022-12-08 06:04:07 -0500

    Activity 1.1

--- src/components/ActivityOne.vue
+++ src/components/ActivityOne.vue
@@ -6,46 +6,43 @@ import { ref } from "vue";
 // function ✅
 let number = ref(0);
 
-function increment() {
-  number.value++;
+function increment(num) {
+  number.value+= num
 }
 
-function decrement() {
-  number.value--;
-}
-
-function increment_by_two() {
-  number.value += 2;
-}
-
-function decrement_by_two() {
-  number.value -= 2;
+function decrement (num) {
+  number.value-= num
 }
 </script>
 
 <template>
   <div class="number">{{ number }}</div>
   <div class="wrapper">
-    <button @click="increment()">Increment</button>
-    <button @click="decrement()">Decrement</button>
-    <button @click="increment_by_two()">Increment by 2</button>
-    <button @click="decrement_by_two()">Decrement by 2</button>
+    <button @click="increment(1)">Increment</button>
+    <button @click="decrement(1)">Decrement</button>
+
+    <button @click="increment(2)">Incrementby2</button>
+    <button @click="decrement(2)">Decrementby2</button>
+    
+    <button @click="increment(3)">Incrementby3</button>
+    <button @click="decrement(3)">Decrementby3</button>
   </div>
+
 </template>
 
 <style>
 .number {
   color: white;
-  font-size: 50px;
+  font-size: 30px;
   font-weight: bold;
   margin-bottom: 20px;
   text-align: center;
   padding: 10px;
-  background-color: green;
+  background-color: darkmagenta;
 }
 
 .wrapper {
   display: flex;
-  grid-gap: 40px;
+  grid-gap: 100px;
 }
 </style>
