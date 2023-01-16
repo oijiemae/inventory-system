59aa803d54db89e76e2fbf82273e56977c189469
pc <krishanicole@ckcm.edu.ph.com>
2023-01-16 15:49:51 +0800

    Calculator

--- src/components/ActivityTwo.vue
+++ src/components/ActivityTwo.vue
@@ -1,341 +1,243 @@
-<script setup>
-const keys = document.querySelectorAll('.key');
-const display_input = document.querySelector('.display .input');
-const display_output = document.querySelector('.display .output');
-
-let input = "";
-
-for (let key of keys) {
-	const value = key.dataset.key;
-
-	key.addEventListener('click', () => {
-		if (value == "clear") {
-			input = "";
-			display_input.innerHTML = "";
-			display_output.innerHTML = "";
-		} else if (value == "backspace") {
-			input = input.slice(0, -1);
-			display_input.innerHTML = CleanInput(input);
-		} else if (value == "=") {
-			let result = eval(PerpareInput(input));
-
-			display_output.innerHTML = CleanOutput(result);
-		} else if (value == "brackets") {
-			if (
-				input.indexOf("+") == -1 || 
-				input.indexOf("+") != -1 && 
-				input.indexOf("-") != -1 && 
-				input.lastIndexOf("+") < input.lastIndexOf("-")
-			) {
-				input += "+";
-			} else if (
-				input.indexOf("+") != -1 && 
-				input.indexOf("-") == -1 || 
-				input.indexOf("+") != -1 &&
-				input.indexOf("-") != -1 &&
-				input.lastIndexOf("+") > input.lastIndexOf("-")
-			) {
-				input += "-";
-			}
-
-			display_input.innerHTML = CleanInput(input);
-		} else {
-			if (ValidateInput(value)) {
-				input += value;
-				display_input.innerHTML = CleanInput(input);
-			}
-		}
-	})
-}
-
-function CleanInput(input) {
-	let input_array = input.split("");
-	let input_array_length = input_array.length;
-
-	for (let i = 0; i < input_array_length; i++) {
-		if (input_array[i] == "*") {
-			input_array[i] = ` <span class="operator">x</span> `;
-		} else if (input_array[i] == "/") {
-			input_array[i] = ` <span class="operator">÷</span> `;
-		} else if (input_array[i] == "+") {
-			input_array[i] = ` <span class="operator">+</span> `;
-		} else if (input_array[i] == "-") {
-			input_array[i] = ` <span class="operator">-</span> `;
-		} else if (input_array[i] == "+") {
-			input_array[i] = `<span class="brackets">+</span>`;
-		} else if (input_array[i] == "-") {
-			input_array[i] = `<span class="brackets">-</span>`;
-		} else if (input_array[i] == "%") {
-			input_array[i] = `<span class="percent">%</span>`;
-		}
-	}
-
-	return input_array.join("");
-}
-
-function CleanOutput (output) {
-	let output_string = output.toString();
-	let decimal = output_string.split(".")[1];
-	output_string = output_string.split(".")[0];
-
-	let output_array = output_string.split("");
-
-	if (output_array.length > 3) {
-		for (let i = output_array.length - 3; i > 0; i -= 3) {
-			output_array.splice(i, 0, ",");
-		}
-	}
-
-	if (decimal) {
-		output_array.push(".");
-		output_array.push(decimal);
-	}
-
-	return output_array.join("");
-}
-
-function ValidateInput (value) {
-	let last_input = input.slice(-1);
-	let operators = ["+", "-", "*", "/"];
-
-	if (value == "." && last_input == ".") {
-		return false;
-	}
-
-	if (operators.includes(value)) {
-		if (operators.includes(last_input)) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	return true;
-}
-
-function PerpareInput (input) {
-	let input_array = input.split("");
-
-	for (let i = 0; i < input_array.length; i++) {
-		if (input_array[i] == "%") {
-			input_array[i] = "/100";
-		}
-	}
-
-	return input_array.join("");
+<script>
+export default {
+  data() {
+      return {
+          input: '',
+          output:'',
+          calculated:false
+      }
+  },
+  methods: {
+      clear() {
+          this.input=''
+          this.output=''
+      },
+      cancel(){
+          //this.input = this.input.slice(0, -1);
+          const arr = this.input.split('')
+          arr.pop()
+          this.input = arr.join('')
+      },
+      enter(keys){
+          if(!this.calculated){
+              this.input +=keys
+          }
+          else{
+              this.calculated = false
+              this.input=''
+              this.output=''
+              this.input +=keys                
+          }
+      },
+      equal(){
+          if(this.input.includes('%')){
+              const arr = this.input.split('')
+              const newArr = []
+              for(let i=0;i<arr.length;i++){
+                  if(arr[i]!='%'){
+                      newArr.push(arr[i])
+                  }
+                  else{
+                      newArr.push('/100')
+                  }
+              }
+              this.input = newArr.join('')
+              console.log(newArr)
+          }
+          console.log(this.input)
+          this.output = eval(this.input)
+          this.calculated = true
+      }
+  },
+  
 }
 </script>
 
 <template>
-	<div class="app">
-
-<div class="calculator">
-
-	<div class="display">
-		<div class="content">
-			<div class="input"></div>
-			<div class="output"></div>
-		</div>
-	</div>
-
-	<div class="keys">
-
-		<div data-key="clear" class="key action">
-			<span>AC</span>
-		</div>
-
-		<div data-key="brackets" class="key action">
-			<span>+/-</span>
-		</div>
-
-		<div data-key="%" class="key action">
-			<span>%</span>
-		</div>
-
-		<div data-key="/" class="key operator">
-			<span>÷</span>
-		</div>
-
-		<div data-key="7" class="key">
-			<span>7</span>
-		</div>
-
-		<div data-key="8" class="key">
-			<span>8</span>
-		</div>
-
-		<div data-key="9" class="key">
-			<span>9</span>
-		</div>
-
-		<div data-key="*" class="key operator">
-			<span>&#215;</span>
-		</div>
-
-		<div data-key="4" class="key">
-			<span>4</span>
-		</div>
-
-		<div data-key="5" class="key">
-			<span>5</span>
-		</div>
-
-		<div data-key="6" class="key">
-			<span>6</span>
-		</div>
-
-		<div data-key="-" class="key operator">
-			<span>-</span>
-		</div>
-
-		<div data-key="1" class="key">
-			<span>1</span>
-		</div>
-
-		<div data-key="2" class="key">
-			<span>2</span>
-		</div>
-
-		<div data-key="3" class="key">
-			<span>3</span>
-		</div>
-
-		<div data-key="+" class="key operator">
-			<span>+</span>
-		</div>
-
-		<div data-key="backspace" class="key">
-			<span>&#8634;</span>
-		</div>
-
-		<div data-key="0" class="key">
-			<span>0</span>
-		</div>
-
-		<div data-key="." class="key">
-			<span>.</span>
-		</div>
-
-		<div data-key="=" class="key operator">
-			<span>=</span>
-		</div>
-	</div>
-
-</div>
-
-</div>
+  <div class="calculator">
+      <div class="display">
+          <div class="input"><span></span>
+              {{input}}
+          </div>
+          <div class="output"><span></span>
+              {{output}}
+          </div>
+      </div>
+      <div class="row-1">
+          <div class="button keys" @click="clear">AC</div>
+          <div class="button keys" @click="cancel">+/-</div>
+          <div class="button keys" @click="enter('%')">%</div>
+          <div class="button operator" @click="enter('/')">÷</div>
+      </div>
+      <div class="row-2">
+          <div class="button" @click="enter(7)">7</div>
+          <div class="button" @click="enter(8)">8</div>
+          <div class="button" @click="enter(9)">9</div>
+          <div class="button operator" @click="enter('*')">*</div>
+      </div>
+      <div class="row-3">
+          <div class="button" @click="enter(4)">4</div>
+          <div class="button" @click="enter(5)">5</div>
+          <div class="button" @click="enter(6)">6</div>
+          <div class="button operator" @click="enter('-')">-</div>
+      </div>
+      <div class="row-4">
+          <div class="button" @click="enter(1)">1</div>
+          <div class="button" @click="enter(2)">2</div>
+          <div class="button" @click="enter(3)">3</div>
+          <div class="button operator" @click="enter('+')">+</div>
+      </div>
+      <div class="row-5">
+          <div class="button" @click="cancel">⟲</div>  
+          <div class="button" @click="enter('0')">0</div>
+          <div class="button operator" @click="enter('.')">.</div>
+          <div class="button operator" @click="equal">=</div>
+      </div>
+  </div>
+  
 </template>
-<style>* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: sans-serif;
-}
 
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
 .app {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
+.calculator{
+  min-height: 640px;
+  max-width: 375px;
+  font-size: 2rem;
+  width:100%;
+  height:30rem;
+  margin:auto;
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+  background-color: rgb white;
+  border: 4px solid rgb(241, 241, 234);
+  border-radius: 5rem;
+  -webkit-border-radius: 2.4rem;
+  -moz-border-radius: 0.4rem;
+  -ms-border-radius: 0.4rem;
+  -o-border-radius: 0.4rem;
+  -webkit-box-shadow: 1rem 1rem 0.5rem rgb white;
+  box-shadow: 0.1rem 0.1rem 0.5rem rgb lightgray;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 
-.calculator {
-	background-color: whitesmoke;
-	width: 100%;
-	max-width: 375px;
-	min-height: 640px;
-	display: flex;
-	flex-direction: column;
-	border-radius: 1.5rem;
-	overflow: hidden;
 }
-
-.display {
-	min-height: 200px;
-	padding: 1.5rem;
-	display: flex;
-	justify-content: flex-end;
-	align-items: flex-end;
-	color: black;
-	text-align: right;
-	flex: 1 1 0%;
+.display{
+  min-width: 20px;
+  font-size:1.8rem;
+  width:80%;
+  height:6px;
+  border: 1px solid rgb white;
+  border-radius: 0.5rem;
+  background-color: rgb white;
+  margin: auto;
+  text-align: right;
+  flex: 10%;
+  margin-top: 8.5rem;
 }
-
-.display .content {
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	width: 100%;
-	max-width: 100%;
-	overflow: auto;
+.row-1,.row-2,.row-3,.row-4,.row-5{
+  width:90%;
+  height:auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin:auto;
+  margin-top: 0.7rem;
+  margin-bottom: 2.5%;
 }
-
-.display .input {
-	width: 100%;
-	font-size: 1.25rem;
-	margin-bottom: 0.5rem;
+.row-1 .button{
+  background-color: #f8f8f8;
 }
-
-.display .output {
-	font-size: 3rem;
-	font-weight: 700;
-	width: 100%;
-	white-space: nowrap;
+.row-2 .button{
+  background-color: #f8f8f8;
 }
-
-.display .operator {
-	color: #EB6666;
+.row-3 .button{
+  background-color: #f8f8f8;
 }
-
-.display .brackets,
-.display .percent {
-	color: #26FED7;
+.row-4 .button{
+  background-color: #f8f8f8;
 }
-
-.keys {
-	background-color: white;
-	padding: 1.5rem;
-	border-radius: 1.5rem 1.5rem 0 0;
-	display: grid;
-	grid-template-columns: repeat(4, 1fr);
-	grid-template-rows: repeat(5, 1fr);
-	grid-gap: 1rem;
-	box-shadow: 0px -2px 16px rgba(0, 0, 0, 0.2);
+.row-5 .button{
+  background-color: #f8f8f8;
 }
-
-.keys .key {
-	position: relative;
+.button{
+  position: relative;
+  width: 20%;
+  max-block-size: 100%;
 	cursor: pointer;
-
 	display: block;
-	height: 0;
-	padding-top: 100%;
-	background-color: whitesmoke;
-	border-radius: 1rem;
+	height: 3rem;
+	padding-top: 0.5%;
+	border-radius: 0.5rem;
 	transition: 0.2s;
 	user-select: none;
+  text-align: center;
+  font-weight: 100%;
+  font-size: 2rem;
 }
 
-.keys .key span {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-
-	font-size: 2rem;
-	font-weight: 700;
-	color: black;
+.button.equal{
+  width:30%;
 }
-
-.keys .key:hover {
-	box-shadow: inset 0px 0px 8px rgba(0, 0, 0, 0.2);
+.button:hover,.button.button.equal:hover{
+  color: rgb white;
+  -webkit-box-shadow: 0.3rem 0.3rem 0.5rem #000000;
+  box-shadow: 0.3rem 0.3rem 0.5rem #000000;
 }
-
-.keys .key.operator span {
-	color: #EB6666;
+.button:active,.button.button.equal:active{
+  transform: translate(0.05rem,0.05rem);
+  -moz-transform: translate(0.05rem,0.05rem);
+  -webkit-transform: translate(0.05rem,0.05rem);
 }
+.button.reload{
+  width: 20%;
+}
+.button.button.button.reload:active{
+  transform: translate(0.05rem,0.05rem);
+  -moz-transform: translate(0.05rem,0.05rem);
+  -webkit-transform: translate(0.05rem,0.05rem);
+}
+.button:hover,.button.button.equal:hover{
+  color: #000000;
+  -webkit-box-shadow: 0.3rem 0.3rem 0.5rem #000000;
+  box-shadow: 0.3rem 0.3rem 0.5rem #000000;
+}
+.input,output{
+  min-width: 280px;
+  width:100%;
+  margin:auto 0;
+}
+.display .input{
+  font-size: 1.60rem;
+  margin-bottom: 0.5rem;
+  color:#000000;
+  font-size: 2rem;
 
-.keys .key.action span {
-	color: #26FED7;
-}</style>
-
+}
+.display .output {
+  color: #000000;
+  font-size: 4rem;
+  font-weight: 700;
+  width: 100%;
+  max-width: 100%;
+  overflow: auto;
+}
+.operator{
+  color: #eb6666 ;
+}
+.keys {
+  color: #0ff ;
+}
+</style>
